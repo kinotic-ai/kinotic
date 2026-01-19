fb1d5f44979d9805c40ff264059dd567aa800624
Gor <muradyangor303@gmail.com>
2026-01-19 13:11:40 +0400

    new and old version switch

--- structures-frontend-next/src/components/modals/StructureDataViewModal.vue
+++ structures-frontend-next/src/components/modals/StructureDataViewModal.vue
@@ -1,14 +1,17 @@
 <script lang="ts">
 import EntityList from "@/pages/EntityList.vue";
+import EntityListOld from "@/components/EntityListOld.vue";
 import { Component, Vue, Prop, Emit } from "vue-facing-decorator";
 
 @Component({
-    components: { EntityList }
+    components: { EntityList, EntityListOld }
 })
 export default class StructureDataViewModal extends Vue {
     @Prop({ default: false }) readonly modelValue!: boolean;
     @Prop({ default: "Data View" }) readonly title!: string;
     @Prop({ default: () => ({}) }) readonly entityProps!: Record<string, unknown>;
+
+    showNewVersion = false;
 
     get visible(): boolean {
         return this.modelValue;
@@ -52,6 +55,10 @@ export default class StructureDataViewModal extends Vue {
         this.visible = false;
         this.emitClose();
     }
+
+    toggleVersion() {
+        this.showNewVersion = !this.showNewVersion;
+    }
 }
 </script>
 <template>
@@ -59,14 +66,23 @@ export default class StructureDataViewModal extends Vue {
         <div class="relative w-full h-screen bg-white shadow-lg overflow-hidden overflow-y-scroll">
             <div class="flex items-center justify-between p-4 border-b border-gray-200">
                 <h3 class="text-xl font-semibold text-gray-900">{{ title }}</h3>
-                <button @click="onHide" class="text-gray-400 hover:text-gray-900 hover:bg-gray-200 rounded-lg text-sm w-8 h-8 flex items-center justify-center">
-                    <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                </button>
+                <div class="flex items-center gap-2">
+                    <button 
+                        @click="toggleVersion" 
+                        class="px-3 py-1.5 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors"
+                    >
+                        {{ showNewVersion ? 'Old Version' : 'New Version' }}
+                    </button>
+                    <button @click="onHide" class="text-gray-400 hover:text-gray-900 hover:bg-gray-200 rounded-lg text-sm w-8 h-8 flex items-center justify-center">
+                        <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                    </button>
+                </div>
             </div>
             <div class="h-full">
-                <EntityList v-bind="entityProps" />
+                <EntityListOld v-if="!showNewVersion" v-bind="entityProps" />
+                <EntityList v-else v-bind="entityProps" />
             </div>
         </div>
     </div>
