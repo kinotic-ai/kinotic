b9d12447dffda55b66a5d55a28db3a82f10cd4a7
Navid Mitchell <navid@mitchellsoftware.net>
2026-02-08 02:51:55 -0600

    Added comments to get rid of warnings

--- structures-migration/src/main/java/org/kinotic/structures/migration/config/MirationProperties.java
+++ structures-migration/src/main/java/org/kinotic/structures/migration/config/MirationProperties.java
@@ -23,19 +23,38 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class MirationProperties {
 
+    /**
+     * The host of the elasticsearch instance to connect to and migrate data.
+     */
     @NotBlank
     private String elasticHost;
 
+    /**
+     * The port of the elasticsearch instance to connect to and migrate data.
+     */
     @NotNull
     private Integer elasticPort;
 
+    /**
+     * The scheme (http or https) of the elasticsearch instance to connect to and migrate data.
+     */
     @NotBlank
     private String elasticScheme;
 
+    /**
+     * The username to use when connecting to the elasticsearch instance to migrate data. Optional, only needed if the elasticsearch instance requires authentication.
+     */
     private String elasticUsername = null;
 
+    /**
+     * The password to use when connecting to the elasticsearch instance to migrate data. Optional, only needed if the elasticsearch instance requires authentication.
+     */
     private String elasticPassword = null;
 
+    /**
+     * Helper method to determine if both elastic username and password are provided and non-blank.
+     * @return true if both elasticUsername and elasticPassword are non-null and not blank, false otherwise.
+     */
     public boolean hasElasticUsernameAndPassword(){
         return elasticUsername != null && !elasticUsername.isBlank() && elasticPassword != null && !elasticPassword.isBlank();
     }
