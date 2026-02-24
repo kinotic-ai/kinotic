1e5fc62305d472739880c8547ab931a9868ae1ab
Navid Mitchell <navid@mitchellsoftware.net>
2026-02-24 14:23:05 -0700

    Renamed kinotic-boot to kinotic-core

--- kinotic-core/src/main/java/org/kinotic/boot/internal/config/KinoticIgniteConfig.java
+++ kinotic-core/src/main/java/org/kinotic/boot/internal/config/KinoticIgniteConfig.java
@@ -2,6 +2,7 @@
 
 package org.kinotic.boot.internal.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ignite.IgniteSystemProperties;
