d56a8699a98f27f1f32a07d7bdab08732c7251b2
NavidMitchell <NavidMitchell@users.noreply.github.com>
2026-04-02 16:16:17 +0000

    deploy: 04195706e90d57467ab6bd5f9a85a3801d04ded6

---
+++ _nuxt/DKELQCNB.js
@@ -0,0 +1,1 @@
+import{r as s}from"./D_zE6B_f.js";import t from"./DNk3-wrW.js";import{_ as a,a as e,p as n,s as p,o as i}from"./CFEq_MpQ.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=a();return(r,f)=>(i(),e(t,{color:"info",icon:p(o).ui.icons.info},{default:n(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
