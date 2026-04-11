a08937103a711afb93d26686a4c033b2a46a1560
Navid Mitchell <navid@mitchellsoftware.net>
2026-04-11 16:42:44 -0600

    Small changes

--- kinotic-js/e2e-tests/test/repository/generated/BasePersonWithTenantRepository.ts
+++ kinotic-js/e2e-tests/test/repository/generated/BasePersonWithTenantRepository.ts
@@ -13,7 +13,7 @@ export class BasePersonWithTenantRepository extends EntityRepository<PersonWithT
   private readonly shouldValidate: boolean
 
   constructor(shouldValidate: boolean = true, entitiesRepository?: IEntitiesRepository) {
-    super('pyEpiAfRQQ', 'PersonWithTenant', entitiesRepository)
+    super('oaikzmgvdO', 'PersonWithTenant', entitiesRepository)
     this.shouldValidate = shouldValidate
   }
 
