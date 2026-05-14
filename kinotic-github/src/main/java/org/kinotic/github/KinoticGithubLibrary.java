864da3be0b7a1cd544e76758ec8a705632a5b0f9
Navid Mitchell <navid@mitchellsoftware.net>
2026-05-13 19:00:57 -0600

    Small change

--- kinotic-github/src/main/java/org/kinotic/github/KinoticGithubLibrary.java
+++ kinotic-github/src/main/java/org/kinotic/github/KinoticGithubLibrary.java
@@ -1,6 +1,5 @@
 package org.kinotic.github;
 
-import org.kinotic.github.api.config.KinoticGithubProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
